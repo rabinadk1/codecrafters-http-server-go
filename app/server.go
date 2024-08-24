60217651b6bd35f9aaec32b554a6c49aa3f079b8
Rabin Adhikari <rabin.adk1@gmail.com>
2024-08-24 11:05:26 +0545

    Feat: Send 200 OK Response

--- app/server.go
+++ app/server.go
@@ -2,20 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
-	"os"
 )
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
+		log.Fatalln("Failed to bind to port 4221")
 	}
+	defer l.Close()
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	fmt.Println("Listening on port 4221")
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatalln("Error accepting connection: ", err.Error())
+		}
+
+		go func(conn net.Conn) {
+			defer conn.Close()
+
+			fmt.Printf("Accepted connection for %s\n", conn.RemoteAddr())
+
+			_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+			if err != nil {
+				log.Fatalf("Error writing response: %s\n", err)
+			}
+		}(conn)
 	}
 }
