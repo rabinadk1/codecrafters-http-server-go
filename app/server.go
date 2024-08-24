2639dee40eac40aa82c3e4d791e02ea568fbb869
Rabin Adhikari <rabin.adk1@gmail.com>
2024-08-24 11:19:26 +0545

    Feat: Send 404 Not Found for giberrish targets

--- app/server.go
+++ app/server.go
@@ -4,7 +4,45 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 )
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	fmt.Printf("Accepted connection for %s\n", conn.RemoteAddr())
+
+	// Buffer to read from the connection
+	buffer := make([]byte, 64)
+
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Printf("Error reading from connection: %s\n", err)
+		return
+	}
+
+	headerParts := strings.SplitN(string(buffer[:n]), " ", 3)
+
+	if len(headerParts) != 3 {
+		fmt.Printf("Invalid request: %s\n", headerParts)
+		return
+	}
+
+	requestTarget := headerParts[1]
+
+	var response string
+	if requestTarget == "/" {
+		response = "HTTP/1.1 200 OK\r\n\r\n"
+	} else {
+		response = "HTTP/1.1 404 Not Found\r\n\r\n "
+	}
+
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Printf("Error writing response: %s\n", err)
+		return
+	}
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -18,18 +56,11 @@ func main() {
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			log.Fatalln("Error accepting connection: ", err.Error())
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
 		}
 
-		go func(conn net.Conn) {
-			defer conn.Close()
+		go handleConnection(conn)
 
-			fmt.Printf("Accepted connection for %s\n", conn.RemoteAddr())
-
-			_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-			if err != nil {
-				log.Fatalf("Error writing response: %s\n", err)
-			}
-		}(conn)
 	}
 }
