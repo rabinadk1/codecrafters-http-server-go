e22d3b73714c96e1f6ce28171467a3fba887c122
Rabin Adhikari <rabin.adk1@gmail.com>
2024-08-24 12:20:05 +0545

    Feat: Read User-Agent header

--- app/server.go
+++ app/server.go
@@ -13,15 +13,17 @@ func handleConnection(conn net.Conn) {
 	fmt.Printf("Accepted connection for %s\n", conn.RemoteAddr())
 
 	// Buffer to read from the connection
-	buffer := make([]byte, 64)
+	buffer := make([]byte, 1204)
 
 	n, err := conn.Read(buffer)
 	if err != nil {
 		fmt.Printf("Error reading from connection: %s\n", err)
 		return
 	}
+	buffer = buffer[:n]
+	stringBuffer := string(buffer)
 
-	headerParts := strings.SplitN(string(buffer[:n]), " ", 3)
+	headerParts := strings.SplitN(stringBuffer, " ", 3)
 
 	if len(headerParts) != 3 {
 		fmt.Printf("Invalid request: %s\n", headerParts)
@@ -37,6 +39,23 @@ func handleConnection(conn net.Conn) {
 		echoBody := strings.TrimPrefix(requestTarget, "/echo/")
 		contentLength := len(echoBody)
 		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", contentLength, echoBody)
+	} else if requestTarget == "/user-agent" {
+		const userAgentPrefix = "User-Agent: "
+		userAgentIndex := strings.Index(stringBuffer, userAgentPrefix)
+		if userAgentIndex == -1 {
+			fmt.Printf("Could not find User-Agent in request: %s\n", stringBuffer)
+		}
+
+		remString := stringBuffer[userAgentIndex+len(userAgentPrefix):]
+
+		rnIndex := strings.Index(remString, "\r\n")
+		if rnIndex == -1 {
+			fmt.Printf("Could not find \\r\\n in request: %s\n", stringBuffer)
+		}
+
+		userAgent := remString[:rnIndex]
+		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)
+
 	} else {
 		response = "HTTP/1.1 404 Not Found\r\n\r\n "
 	}
